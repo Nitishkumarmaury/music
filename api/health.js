f71fe849e751056cc11661ef2f9435055d8b4457
Nitishkumarmaury <nitishmaurya6580@gmail.com>
2025-08-31 07:43:16 +0530

    Force Vercel redeploy: update api/health.js to trigger function deployment

--- api/health.js
+++ api/health.js
@@ -1,4 +1,5 @@
 // api/health.js (Vercel serverless function)
+// Trivial change to force Vercel redeploy (2025-08-31)
 module.exports = (req, res) => {
   res.status(200).json({ status: 'ok' });
 };
